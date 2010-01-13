6a1ee0054033f7a53152a7db703f5942df389609
Mark Gius <mgius7096@gmail.com>
2010-01-13 20:50:20 +0000

    Alright, be a dick about it.  Lab3 done
    git-svn-id: file:///home/mgius/svn/cpe471@21 b4bbb0b0-33be-4041-8e01-66090f03443a

--- lab3/simple_release.cpp
+++ lab3/simple_release.cpp
@@ -1,30 +1,29 @@
 //A very simple opengl/GLUT program
 #include <GL/glut.h>
+#include <math.h>
 #include <stdlib.h>
 #include <stdio.h>
 
 #include <vector>
 
+using namespace std;
 //global variables - necessary evil when using GLUT
 //global width and height
 int GW;
 int GH;
 //current mouse position in pixel coordinate
-int x;
-int y;
+//int x;
+//int y;
 
-//a structure to store 2D points  - this could also be a class
-typedef struct vector2 {
+typedef struct Point {
   float x;
   float y;
-} vector2;
+  Point(float x_, float y_) {
+     x = x_;
+     y = y_;
+  }
+} Point;
 
-//an array to store ten 2D points
-//vector2 ten_pts[10];
-std::vector<vector2> pts;
-//keep track of the number of points stored thus far
-//keep track of which drawing mode we are in points or square
-//mode ==0 means draw square otherwise draw points
 int mode;
 
 //#define WorldW 2.0
@@ -38,40 +37,75 @@ inline float p2w_x(float w) {
 inline float p2w_y(float h) {
    return -1 * (WorldH / GH * h - WorldH / 2);
 }
-//helper drawing routines
-void draw_square() {
-  glBegin(GL_POLYGON);
-    glColor3f(1.0, 0.0, 0.0);
-		glVertex2f(-0.5,-0.5); 
-    glColor3f(0.0, 1.0, 0.0); 
-		glVertex2f(-0.5,0.5);
-	  glColor3f(0.0, 0.0, 1.0);
-		glVertex2f(0.5,0.5); 
-		glColor3f(0.0, 0.0, 0.0);  
-		glVertex2f(0.5,-0.5);
-	glEnd();
+
+inline float deg2rad(int deg) {
+   return M_PI * deg / 180.0;
 }
 
-void draw_pts() {
-  glPointSize(2.0);
-  glBegin(GL_POINTS);
-    glColor3f(0.5, 0.2, 0.8);
-  for (int i=0; i<pts.size(); i++) {
-    glVertex2f(pts[i].x, pts[i].y);
-    //printf("pt: %f %f\n", ten_pts[i].x, ten_pts[i].y);
-  }
-  glEnd();
-}
+#define SHAPE_CIRCLE 1
+#define SHAPE_LINE   2
+
+class stroke {
+   vector<Point> points;
+   int shape; // shape of stroke
+
+public:
+   stroke() : shape(mode) {
+   }
+
+   void addPoint(Point aPoint) {
+      points.push_back(aPoint);
+   }
+   // Draws a stroke to the current window
+   void draw() {
+      if (shape == SHAPE_CIRCLE)
+         drawCircles();
+      else if(shape == SHAPE_LINE)
+         drawLines();
+      else
+         printf("Not a valid stroke type, ignoring stroke...\n");
+   }
+private:
+   void drawCircles() {
+      printf("Drawing circles\n");
+      for (int j = 0; j < points.size(); j++) {
+         glBegin(GL_TRIANGLE_FAN);
+         glColor3f(1.0,0.0,0.0);
+         glVertex2f(points[j].x,points[j].y);
+         printf("Drawing a circle at %f, %f\n");
+         for (int i = 0; i < 31; i++) {
+            glVertex2f(.1 * cos(deg2rad(i*12)) + points[j].x,
+                  .1 * sin(deg2rad(i*12)) + points[j].y);
+         }
+         glEnd();
+
+      }
+   }
+
+   void drawLines() {
+      printf("Drawing Lines\n");
+      glBegin(GL_LINES);
+      glColor3f(0.0,1.0,0.0);
+      // floor points.size / 2 so if uneven number of points the last
+      // gets dropped
+      for (int i = 0; i < (points.size() / 2); i++) {
+         glVertex2f(points[2 * i].x, points[2 * i].y);
+         glVertex2f(points[2 * i + 1].x, points[2 * i + 1].y);
+      }
+      glEnd();
+   }
+
+};
+vector<stroke> strokes;
 
 
 //the display call back - all drawing should be done in this function
 void display() {
 	glClear(GL_COLOR_BUFFER_BIT);
 
-  if (mode == 0)
-    draw_square();
-  else
-    draw_pts();
+   for (int i = 0; i < strokes.size(); i++) {
+      strokes[i].draw();
+   }
 
 	glFlush();
 }
@@ -81,12 +115,9 @@ void mouse(int button, int state, int x, int y) {
   if (button == GLUT_LEFT_BUTTON) {
     if (state == GLUT_DOWN) { /* if the left button is clicked */
       printf("mouse clicked at %d %d (%f, %f)\n", x, y, p2w_x(x), p2w_y(y) );
-        vector2 temp;
-        temp.x = p2w_x(x);
-        temp.y = p2w_y(y);
 
-        pts.push_back(temp);
-        glutPostRedisplay();
+      strokes.back().addPoint(Point(p2w_x(x),p2w_y(y)));
+      glutPostRedisplay();
 
     }
   }
@@ -95,30 +126,19 @@ void mouse(int button, int state, int x, int y) {
 //the mouse move callback
 void mouseMove(int x, int y) {
   printf("mouse moved at %d %d\n", x, y);
-  vector2 temp;
-  temp.x = p2w_x(x);
-  temp.y = p2w_y(y);
+  // shouldn't do anything on move
 
-  pts.push_back(temp);
-  glutPostRedisplay();
 }
 
 //the keyboard callback
 void keyboard(unsigned char key, int x, int y ){
   switch( key ) {
-    case 'm':
-      mode = !mode;
-      glutPostRedisplay();
-      break;
     case 'q': case 'Q' :
       exit( EXIT_SUCCESS );
       break;
-    case 'h' : case 'H' :
-      printf("hello!\n");
-      break;
     case 'c' : case 'C' :
       printf("Clearing points\n");
-      pts.clear();
+      strokes.clear();
       glutPostRedisplay();
       break;
   }
@@ -133,20 +153,24 @@ void reshape( GLsizei w, GLsizei h) {
    gluOrtho2D(-(float)w/h, (float)w/h, -1, 1);
 }
 
+#define LINE_MODE 1
+#define CIRCLE_MODE 2   
+#define CLEAR 4
 void menu ( int value) {
-   if (value == 1) {
-      printf("Switching mode");
-      mode = !mode;
-      glutPostRedisplay();
+   if (value == LINE_MODE) {
+      printf("Line mode\n");
+      mode = SHAPE_LINE;
+      strokes.push_back(stroke());
    }
-   else if (value == 2) {
+   else if (value == CIRCLE_MODE) {
+      printf("Circle mode\n");
+      mode = SHAPE_CIRCLE;
+      strokes.push_back(stroke());
+   }
+   else if (value == CLEAR) {
       printf("clear");
-      pts.clear();
+      strokes.clear();
       glutPostRedisplay();
-   }
-   else if (value == 3) {
-      printf("quit");
-      exit( EXIT_SUCCESS );
    }
 }
 
@@ -157,11 +181,12 @@ int main( int argc, char** argv ){
   //intializations
 	glutInitWindowSize(200, 200);
 	glutInitWindowPosition(100, 100);
-	glutCreateWindow( "My Second Window" );
+	glutCreateWindow( "My Third Window" );
 	glClearColor(1.0, 1.0, 1.0, 1.0);
   //global variable intialization
   GW = GH = 200;
-  mode = 0;
+  mode = SHAPE_CIRCLE;
+  strokes.push_back(stroke());
   //register the callback functions
 	glutDisplayFunc( display );
   glutMouseFunc( mouse );
@@ -171,8 +196,9 @@ int main( int argc, char** argv ){
 
   // menu
   int rightMenu = glutCreateMenu(menu);
-  glutAddMenuEntry("Toggle Mode", 1);
-  glutAddMenuEntry("Clear Points", 2);
+  glutAddMenuEntry("Line Mode", LINE_MODE);
+  glutAddMenuEntry("Circle Mode", CIRCLE_MODE);
+  glutAddMenuEntry("Clear Points", CLEAR);
   glutAddMenuEntry("Quit", 3);
   glutAttachMenu(GLUT_RIGHT_BUTTON);
 
