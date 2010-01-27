1c8bb7f4e3c21b910631230fa22b9e9fb277adc6
Mark Gius <mgius7096@gmail.com>
2010-01-27 21:19:43 +0000

    Lab5 done
    git-svn-id: file:///home/mgius/svn/cpe471@41 b4bbb0b0-33be-4041-8e01-66090f03443a

--- lab5/simple_release.cpp
+++ lab5/simple_release.cpp
@@ -16,6 +16,12 @@ int GH;
 // current rotation and position of triangle
 float theta = 0.0;
 float xPos = 0.0, yPos = 0.0;
+bool rotate = false;
+bool move = false;
+
+#define RED 0
+#define GREEN 1
+#define BLUE 2
 
 //#define WorldW 2.0
 #define WorldW 2 *(float)GW / (float)GH
@@ -34,10 +40,11 @@ inline float deg2rad(int deg) {
 }
 
 void idle(int) {
-	theta += 1.0;
+	theta += 2.0;
 	if (theta >= 360.0)
 		theta-=360.0;
-	glutTimerFunc(100, idle, 0);
+	if (rotate)
+		glutTimerFunc(100, idle, 0);
 	glutPostRedisplay();
 }
 
@@ -45,36 +52,62 @@ void idle(int) {
 //the display call back - all drawing should be done in this function
 #define TRIANGLE_SIDE_LENGTH 1.0
 void display() {
-	printf("display %f\n", theta);
 	glClear(GL_COLOR_BUFFER_BIT);
 	glPushMatrix();
-	glRotatef(theta, 0, 0, 1);
-	glBegin(GL_TRIANGLES);
-		glColor3f(1.0, 0.0, 0.0);
-		glVertex2f(cos(deg2rad(0)), sin(deg2rad(0)));
-		glVertex2f(cos(deg2rad(120)), sin(deg2rad(120)));
-		glVertex2f(cos(deg2rad(240)), sin(deg2rad(240)));
-	glEnd();
+	{
+		glTranslatef(xPos, yPos, 0);
+		glRotatef(theta, 0, 0, 1);
+		glBegin(GL_TRIANGLES);
+		{
+			glColor3f(1.0, 0.0, 0.0);
+			glVertex2f(0.2 * cos(deg2rad(0)), 0.2 * sin(deg2rad(0)));
+			glVertex2f(0.2 * cos(deg2rad(120)), 0.2 * sin(deg2rad(120)));
+			glVertex2f(0.2 * cos(deg2rad(240)), 0.2 * sin(deg2rad(240)));
+		}
+		glEnd();
+	}
 	glPopMatrix();
 
 	glutSwapBuffers();
 }
+
+int lastMouseX, lastMouseY;
 
 //the mouse button callback
 void mouse(int button, int state, int x, int y) {
   if (button == GLUT_LEFT_BUTTON) {
     if (state == GLUT_DOWN) { /* if the left button is clicked */
       printf("mouse clicked at %d %d (%f, %f)\n", x, y, p2w_x(x), p2w_y(y) );
+		float pixBuf[3];
+		glReadPixels(x, GH - y, 1,1, GL_RGB, GL_FLOAT, &pixBuf);
+		if (pixBuf[RED] == 1.0 &&
+			 pixBuf[BLUE] == 0.0 &&
+			 pixBuf[GREEN] == 0.0) {
+			move = true;
+			lastMouseX = x;
+			lastMouseY = y;
+		}
+
 
       glutPostRedisplay();
 
     }
+	 if (state == GLUT_UP) {
+		 move = false;
+	 }
   }
 }
 
 //the mouse move callback
 void mouseMove(int x, int y) {
   printf("mouse moved at %d %d\n", x, y);
+  if (move) {
+	  xPos -= p2w_x(lastMouseX) - p2w_x(x);
+	  yPos -= p2w_y(lastMouseY) - p2w_y(y);
+	  lastMouseX = x;
+	  lastMouseY = y;
+	  glutPostRedisplay();
+  }
   // shouldn't do anything on move
 
 }
@@ -85,6 +118,11 @@ void keyboard(unsigned char key, int x, int y ){
     case 'q': case 'Q' :
       exit( EXIT_SUCCESS );
       break;
+	 case 'r': case 'R' :
+		rotate = !rotate;
+		if (rotate)
+			glutTimerFunc(1, idle, 0);
+		break;
     case 'c' : case 'C' :
       glutPostRedisplay();
       break;
@@ -117,7 +155,6 @@ int main( int argc, char** argv ){
   glutMotionFunc( mouseMove );
   glutKeyboardFunc( keyboard );
   glutReshapeFunc( reshape );
-  glutTimerFunc( 1, idle, 0 );
 
 	glutMainLoop();
 }
