fe6c63c79d5d639b02f53cea56f02c79483f6d10
Mark Gius <mgius7096@gmail.com>
2010-02-18 06:44:42 +0000

    All done
    git-svn-id: file:///home/mgius/svn/cpe471@80 b4bbb0b0-33be-4041-8e01-66090f03443a

--- program3/program3.cpp
+++ program3/program3.cpp
@@ -303,92 +303,110 @@ void display() {
 int lastMouseX, lastMouseY;
 
 void mouse(int button, int state, int x, int y) {
-	// Rotate
-	if (button == GLUT_LEFT_BUTTON) {
-		if (state == GLUT_DOWN) { /* if the left button is clicked */
-			printf("left mouse clicked at %d %d\n", x, y);
-			mouseMove_mode = MODE_ROTATE;
-			startClick = Vector3D(p2w_x(x), p2w_y(y),0);
-			startClick.scaleToOne();
-			startClick.bindZ();
-			endClick = Vector3D(p2w_x(x), p2w_y(y),0);
-			endClick.scaleToOne();
-			endClick.bindZ();
+	if (mouse_mode == MODE_TRACKBALL) {
+		// Rotate
+		if (button == GLUT_LEFT_BUTTON) {
+			if (state == GLUT_DOWN) { /* if the left button is clicked */
+				printf("left mouse clicked at %d %d\n", x, y);
+				mouseMove_mode = MODE_ROTATE;
+				startClick = Vector3D(p2w_x(x), p2w_y(y),0);
+				startClick.scaleToOne();
+				startClick.bindZ();
+				endClick = Vector3D(p2w_x(x), p2w_y(y),0);
+				endClick.scaleToOne();
+				endClick.bindZ();
+			}
+			if (state == GLUT_UP) {
+			}
 		}
-		if (state == GLUT_UP) {
+		// Move (translate)
+		if (button == GLUT_RIGHT_BUTTON) {
+			if (state == GLUT_DOWN) { /* if the left button is clicked */
+				printf("right mouse clicked at %d %d\n", x, y);
+				mouseMove_mode = MODE_TRANSLATE;
+
+				lastMouseX = x;
+				lastMouseY = y;
+			}
+			if (state == GLUT_UP) {
+
+			}
+
+		}
+		// Zoom (scale)
+		if (button == GLUT_MIDDLE_BUTTON) {
+			if (state == GLUT_DOWN) { /* if the middle button is clicked */
+				printf("middle mouse clicked at %d %d\n", x, y);
+				mouseMove_mode = MODE_SCALE;
+				lastMouseX = x;
+			}
+			if (state == GLUT_UP) {
+
+			}
 		}
 	}
-	// Move (translate)
-	if (button == GLUT_RIGHT_BUTTON) {
-		if (state == GLUT_DOWN) { /* if the left button is clicked */
-			printf("right mouse clicked at %d %d\n", x, y);
-			mouseMove_mode = MODE_TRANSLATE;
-
+	else if (mouse_mode == MODE_LIGHT) {
+		if (button == GLUT_LEFT_BUTTON) {
 			lastMouseX = x;
 			lastMouseY = y;
-		}
-		if (state == GLUT_UP) {
-
-		}
-
-	}
-	// Zoom (scale)
-	if (button == GLUT_MIDDLE_BUTTON) {
-		if (state == GLUT_DOWN) { /* if the middle button is clicked */
-			printf("middle mouse clicked at %d %d\n", x, y);
-			mouseMove_mode = MODE_SCALE;
-			lastMouseX = x;
-		}
-		if (state == GLUT_UP) {
-
 		}
 	}
 }
 
 void mouseMove(int x, int y) {
 	//printf("mouse moved at %d %d\n", x, y);
-	if (mouseMove_mode == MODE_TRANSLATE) {
-		translateM[0] -= p2w_x(lastMouseX) - p2w_x(x);
-		translateM[1] -= p2w_y(lastMouseY) - p2w_y(y);
+	if (mouse_mode == MODE_TRACKBALL) {
+		if (mouseMove_mode == MODE_TRANSLATE) {
+			translateM[0] -= p2w_x(lastMouseX) - p2w_x(x);
+			translateM[1] -= p2w_y(lastMouseY) - p2w_y(y);
+			lastMouseX = x;
+			lastMouseY = y;
+			glutPostRedisplay();
+		}
+		else if (mouseMove_mode == MODE_SCALE) {
+			if (lastMouseX > x) {
+				// moving left, shrink
+				scaleM[0] = scaleM[1] -= .01;
+				if (scaleM[0] <= 0.1) {
+					scaleM[0] = scaleM[1] = .1;
+				}
+			}
+			else {
+				//moving right, grow
+				scaleM[0] = scaleM[1] += .01;
+				if (scaleM[0] >= 3.0) {
+					scaleM[0] = scaleM[1] = 3.0;
+				}
+			}
+			lastMouseX = x;
+			glutPostRedisplay();
+		}
+		else if (mouseMove_mode == MODE_ROTATE) {
+			endClick = Vector3D(p2w_x(x), p2w_y(y), 0);
+			endClick.scaleToOne();
+			endClick.bindZ();
+			Vector3D axisRot = startClick.crossProd(endClick);
+			float angleInDeg = rad2deg(findAngle(startClick, endClick));
+			//cout << "start: " << startClick <<  " length: " << startClick.length() <<
+			//	cout << "end: " << endClick <<  " length: " << endClick.length() << endl;
+			//cout << "axis: " << axisRot << endl;
+			glMatrixMode(GL_MODELVIEW);
+			glPushMatrix(); {
+				glLoadIdentity();
+				glRotatef(angleInDeg, axisRot.getX(), axisRot.getY(), axisRot.getZ());
+				glMultMatrixf(trackballM);
+				glGetFloatv(GL_MODELVIEW_MATRIX, trackballM);
+			} glPopMatrix();
+			startClick = endClick;
+			glutPostRedisplay();
+		}
+	}
+	else if(mouse_mode == MODE_LIGHT) {
+		light_pos[0] -= p2w_x(lastMouseX) - p2w_x(x);
+		light_pos[1] -= p2w_y(lastMouseY) - p2w_y(y);
 		lastMouseX = x;
 		lastMouseY = y;
-		glutPostRedisplay();
-	}
-	else if (mouseMove_mode == MODE_SCALE) {
-		if (lastMouseX > x) {
-			// moving left, shrink
-			scaleM[0] = scaleM[1] -= .01;
-			if (scaleM[0] <= 0.1) {
-				scaleM[0] = scaleM[1] = .1;
-			}
-		}
-		else {
-			//moving right, grow
-			scaleM[0] = scaleM[1] += .01;
-			if (scaleM[0] >= 3.0) {
-				scaleM[0] = scaleM[1] = 3.0;
-			}
-		}
-		lastMouseX = x;
-		glutPostRedisplay();
-	}
-	else if (mouseMove_mode == MODE_ROTATE) {
-		endClick = Vector3D(p2w_x(x), p2w_y(y), 0);
-		endClick.scaleToOne();
-		endClick.bindZ();
-		Vector3D axisRot = startClick.crossProd(endClick);
-		float angleInDeg = rad2deg(findAngle(startClick, endClick));
-		//cout << "start: " << startClick <<  " length: " << startClick.length() <<
-		//	cout << "end: " << endClick <<  " length: " << endClick.length() << endl;
-		//cout << "axis: " << axisRot << endl;
-		glMatrixMode(GL_MODELVIEW);
-		glPushMatrix(); {
-			glLoadIdentity();
-			glRotatef(angleInDeg, axisRot.getX(), axisRot.getY(), axisRot.getZ());
-			glMultMatrixf(trackballM);
-			glGetFloatv(GL_MODELVIEW_MATRIX, trackballM);
-		} glPopMatrix();
-		startClick = endClick;
+		//printf("light at x: %f, y: %f\n", light_pos[0], light_pos[1]);
 		glutPostRedisplay();
 	}
 }
