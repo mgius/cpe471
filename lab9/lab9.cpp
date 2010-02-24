119df2b128a9b839b8e88f9f74050390a5482482
Mark Gius <mgius7096@gmail.com>
2010-02-24 19:57:37 +0000

    Adding initial lab9
    git-svn-id: file:///home/mgius/svn/cpe471@86 b4bbb0b0-33be-4041-8e01-66090f03443a

---
+++ lab9/lab9.cpp
@@ -0,0 +1,136 @@
+//A very simple opengl/GLUT program
+#include <GL/glut.h>
+#include <math.h>
+#include <stdlib.h>
+#include <stdio.h>
+
+using namespace std;
+//global variables - necessary evil when using GLUT
+//global width and height
+int GW;
+int GH;
+
+// Location of the shoulder
+float armPosX = 0, armPosY = 0;
+// How far off perfectly straight the forearm should be roatated
+float foreArmRotate = 10.0;
+
+
+#define RED 0
+#define GREEN 1
+#define BLUE 2
+
+//#define WorldW 2.0
+#define WorldW 2 *(float)GW / (float)GH
+#define WorldH 2.0
+inline float p2w_x(float w) {
+   //float WorldW = 2 * (float) GW / (float) GH;
+   return WorldW / GW * w - WorldW / 2;
+}
+
+inline float p2w_y(float h) {
+   return -1 * (WorldH / GH * h - WorldH / 2);
+}
+
+inline float deg2rad(int deg) {
+   return M_PI * deg / 180.0;
+}
+
+void idle() {
+	foreArmRotate += 1.0;
+	glutPostRedisplay();
+}
+
+// Draws a two part arm using rectangles
+void drawArm() {
+	glColor3f(1.0, 0.0, 0.0);
+	glRectf( -.05, -.25, .05, .25);
+	glPushMatrix(); {
+		glTranslatef(.1, .1, .1);
+		glRotatef(deg2rad(foreArmRotate), 0,0,1);
+		glColor3f(0.0,1.0, 0.0);
+		glRectf(-.05, 0, .05, .50);
+	} glPopMatrix();
+}
+
+
+
+//the display call back - all drawing should be done in this function
+void display() {
+	glClear(GL_COLOR_BUFFER_BIT);
+//	glPushMatrix();
+//	{
+//		glTranslatef(xPos, yPos, 0);
+//		glRotatef(theta, 0, 0, 1);
+//		glBegin(GL_TRIANGLES);
+//		{
+//			glColor3f(1.0, 0.0, 0.0);
+//			glVertex2f(0.2 * cos(deg2rad(0)), 0.2 * sin(deg2rad(0)));
+//			glVertex2f(0.2 * cos(deg2rad(120)), 0.2 * sin(deg2rad(120)));
+//			glVertex2f(0.2 * cos(deg2rad(240)), 0.2 * sin(deg2rad(240)));
+//		}
+//		glEnd();
+//	}
+//	glPopMatrix();
+
+	drawArm();
+
+	glutSwapBuffers();
+}
+
+int lastMouseX, lastMouseY;
+
+//the mouse button callback
+void mouse(int button, int state, int x, int y) {
+  if (button == GLUT_LEFT_BUTTON) {
+    if (state == GLUT_DOWN) { /* if the left button is clicked */
+	 }
+	 if (state == GLUT_UP) {
+	 }
+  }
+}
+
+//the mouse move callback
+void mouseMove(int x, int y) {
+
+}
+
+//the keyboard callback
+void keyboard(unsigned char key, int x, int y ){
+  switch( key ) {
+    case 'q': case 'Q' :
+      exit( EXIT_SUCCESS );
+      break;
+  }
+}
+
+void reshape( GLsizei w, GLsizei h) {
+   GW = w;
+   GH = h;
+   printf("new window size: %d, %d\n", w, h);
+   glViewport(0,0,w,h); // set viewport
+   glLoadIdentity(); // ?
+   gluOrtho2D(-(float)w/h, (float)w/h, -1, 1);
+}
+
+int main( int argc, char** argv ){
+
+	glutInit( &argc, argv );
+	glutInitDisplayMode( GLUT_RGB | GLUT_DOUBLE);
+  //intializations
+	glutInitWindowSize(200, 200);
+	glutInitWindowPosition(100, 100);
+	glutCreateWindow( "My Third Window" );
+	glClearColor(1.0, 1.0, 1.0, 1.0);
+  //global variable intialization
+  GW = GH = 200;
+  //register the callback functions
+	glutDisplayFunc( display );
+  glutMouseFunc( mouse );
+  glutMotionFunc( mouseMove );
+  glutKeyboardFunc( keyboard );
+  glutReshapeFunc( reshape );
+  glutIdleFunc( idle );
+
+	glutMainLoop();
+}
